61beedda73a2f8dffd3baa5bdb8bdbac3b1d54ad
Sazedul Haque <sazedulhaque5@gmail.com>
2019-05-27 15:32:40 +0600

    implement axios

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,10 +1,25 @@
 <template>
 	<div>
 		<h1>Posts</h1>
+		<!-- {{ posts }} -->
+		<div v-for="post in posts">
+			<h2>{{ post.title }}</h2>
+			<p>{{ post.body }}</p>
+		</div>
 	</div>
 </template>
 <script>
 	export default {
-		name: 'posts'
+		name: 'posts',
+		data() {
+			return {
+				posts: {},
+			}
+		},
+		mounted () {
+		    this.axios
+		      	.get('https://jsonplaceholder.typicode.com/posts')
+		      	.then(response => (this.posts = response.data))
+		}
 	}
 </script>
